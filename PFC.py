ec439b4d14ddab305ed20a4f8589f450946e0892
eliana817 <117195284+eliana817@users.noreply.github.com>
2022-11-03 15:03:42 +0100

    Update PFC.py

--- PFC.py
+++ PFC.py
@@ -1,3 +1,23 @@
 import random
+import time
 
 liste = ["Pierre", "Feuille", "Ciseaux"]
+
+nom = str(input("Entrez votre nom :\n"))
+print("")
+
+valeurJoueur = str(input("Entrez Pierre, Feuille ou Ciseaux :\n"))
+print("")
+
+while valeurJoueur != "Pierre" and valeurJoueur != "Feuille" and valeurJoueur != "Ciseaux" :
+  valeurJoueur != str(input("Soyez certain de bien entrer une de ces valeurs : Pierre, Feuille ou Ciseaux"))
+  print("")
+  
+print("{} joue contre ...".format(valeurJoueur))
+print("")
+
+time.sleep(2)
+valeurOrdi = random.choice(liste)
+print(valeurOrdi)
+
+
