9ab4c2fe854dbe77fc42e5ca579126126e6a545a
eliana817 <egrasser@guardiaschool.fr>
2022-11-04 07:29:25 -0700

    couleurs

--- Morpion.py
+++ Morpion.py
@@ -4,31 +4,31 @@ import sys
 
 print("Initialisation du jeu... ")
 
-#def main(main_screen) :
-   # raise Exception
-
+def main(main_screen) :
+   raise Exception
 
 screen = curses.initscr()
-
 curses.curs_set(0)
+
+#if curses.has_colors():
+    #curses.start_color()
+
+
+if (curses.has_colors() == True) :
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_MAGENTA)
+    screen.bkgd(' ', curses.color_pair(1) | curses.A_BOLD)
+
+
+
 screen.addstr(1, 38, "Morpion")
-
-
-
-
-
-
-
-
-
 
 
 screen.addstr(22, 20, "Appuyez sur n'importe quel touche  pour quitter")
 
 screen.refresh()
 
-
-c = screen.getch()
+screen.getch()
 
 
 curses.endwin()
@@ -36,5 +36,5 @@ curses.endwin()
 
 print("Vous avez fermez le jeu.")
 
-#wrapper(main)
+wrapper(main)
 
