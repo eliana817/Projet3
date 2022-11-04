16052ca988eda1c4d44cb8c908b9ed0ce80669b7
eliana817 <egrasser@guardiaschool.fr>
2022-11-04 04:40:23 -0700

    init_screen_curses

--- Morpion.py
+++ Morpion.py
@@ -1,1 +1,15 @@
+import curses
+import sys
+
+print("Initialisation du jeu... ")
+
+screen = curses.initscr()
+
+screen.refresh()
+
+curses.napms(5000)
+curses.endwin()
+
+print("Vous avez fermez le jeu.")
+
 
