8922ce1aad7953ee309458d9d7011ef983d931ab
eliana817 <egrasser@guardiaschool.fr>
2022-11-04 06:24:27 -0700

    Titre_quitter

--- Morpion.py
+++ Morpion.py
@@ -1,15 +1,40 @@
 import curses
+from curses import wrapper
 import sys
 
 print("Initialisation du jeu... ")
 
+#def main(main_screen) :
+   # raise Exception
+
+
 screen = curses.initscr()
+
+curses.curs_set(0)
+screen.addstr(1, 38, "Morpion")
+
+
+
+
+
+
+
+
+
+
+
+screen.addstr(22, 20, "Appuyez sur n'importe quel touche  pour quitter")
 
 screen.refresh()
 
-curses.napms(5000)
+
+c = screen.getch()
+
+
 curses.endwin()
+ 
 
 print("Vous avez fermez le jeu.")
 
+#wrapper(main)
 
