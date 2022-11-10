1c87f834eedf218982bdf6893105fa8b1865ce9c
eliana817 <egrasser@guardiaschool.fr>
2022-11-10 01:03:24 -0800

    description du choix du BOT

--- PFC_final.py
+++ PFC_final.py
@@ -6,47 +6,59 @@ player_name=input('Quel est ton nom ? ')
 print("")
 computer_name=("BOT Chifoumi")
 
-#computer = list[randint(0,2)]
-
 player = True
 
 def unJoueur(player) :
-    scoreBOT = 0
-    scorePlayer = 0
+    score_BOT = 0
+    score_Player = 0
     while player == True:
         computer = list[randint(0,2)]
         player = input('Pierre, Feuille ou Ciseaux ? ')
         print("")
         if player == computer:
-            print("Egalité !")
+            print("BOT Chifoumi a joué {}".format(computer))
+            print("")
+            print("Egalité ! Le BOT Chifoumi a pensé comme toi !")
             print("")
         elif player == "Pierre" or player == "pierre":
             if computer == "Feuille":
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as perdu !", computer_name, "a contré", player_name)
                 print("")
-                scoreBOT += 1
+                score_BOT += 1
             else:
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as gagné !", player_name, "a écrasé", computer_name)
                 print("")
-                scorePlayer += 1
+                score_Player += 1
         elif player == "Feuille" or player == "feuille" :
             if computer == "Ciseaux":
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as perdu...", computer_name, "a découpé", player_name)
                 print("")
-                scoreBOT += 1
+                score_BOT += 1
             else:
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as gagné !", player_name, "a contré", computer_name)
                 print("")
-                scorePlayer += 1
+                score_Player += 1
         elif player == "Ciseaux" or player == "ciseaux":
             if computer == "Pierre":
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as perdu...", computer_name, "a écrasé", player_name)
                 print("")
-                scoreBOT += 1
+                score_BOT += 1
             else:
+                print("BOT Chifoumi a joué {}".format(computer))
+                print("")
                 print("Tu as gagné !", player_name, "a découpé", computer_name)
                 print("")
-                scorePlayer += 1
+                score_Player += 1
         else:
             print("Attention tu as mal fais ton choix, alors veilles à bien écrire ce que tu souhaites jouer.")
             print("")           
@@ -62,20 +74,19 @@ def unJoueur(player) :
             player = False
             print("Vous avez quitté le jeu car vous n'avez pas répondu correctement")
             print("")
-            print("Score = {}({}) à {}({})".format(scorePlayer,player_name, scoreBOT, "BOT Chifoumi"))
+            print("Score = {}({}) à {}({})".format(score_Player,player_name, score_BOT, "BOT Chifoumi"))
             return
 
-    if scorePlayer > scoreBOT :
-        print("Score = {}({}) à {}({}). Vous avez gagné !".format(scorePlayer, player_name, scoreBOT,  "BOT Chifoumi"))
-    elif scorePlayer < scoreBOT :
-        print("Score = {}({}) à {}({}). Vous avez perdu !".format(scorePlayer, player_name, scoreBOT, "BOT Chifoumi"))
+    if score_Player > score_BOT :
+        print("Score = {}({}) à {}({}). Vous avez gagné !".format(score_Player, player_name, score_BOT,  "BOT Chifoumi"))
+    elif score_Player < score_BOT :
+        print("Score = {}({}) à {}({}). Vous avez perdu !".format(score_Player, player_name, score_BOT, "BOT Chifoumi"))
     else :
-        print("Egalité ! Score = {}({}) à {}({}).".format(scorePlayer, player_name, scoreBOT, "BOT Chifoumi")) 
+        print("Egalité ! Score = {}({}) à {}({}).".format(score_Player, player_name, score_BOT, "BOT Chifoumi")) 
 
 unJoueur(player)
     
-    #player = True
-    #computer = list[randint(0,2)]
+    
 
 
 
